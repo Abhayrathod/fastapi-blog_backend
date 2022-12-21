46e52b2d2355db782bfc2a5815fb6efefa53c30d
Tarun <tarunsengar1987@gmail.com>
2022-12-21 23:14:47 +0530

    Changed .env file

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     access_token_expire_minutes: int
     secret_key: str
 
-    # class Config:
-    #     env_file = ".env"   
+    class Config:
+        env_file = ".env"   
 
 settings = Settings()
